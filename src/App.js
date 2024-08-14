590385107e96a873523e915b5acdac686ba2f407
taeyeon0721 <tyk0721@naver.com>
2024-08-14 09:00:00 +0900

    Create main branch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from "./component/Box";
+import {useState} from "react";
+//1. 박스 2개 (타이틀, 사진, 결과값)
+//2. 가위 바위 보 버튼이 있다.
+//3. 버튼을 클릭하면 클릭한 값이 박스에 보임
+//4. 컴퓨터는 랜덤하게 아이템 선택이 된다.
+//5. 3,4번의 결과를 가지고 누가 이겼는지 승패를 따진다.
+//6. 승패 결과에 따라 테두리 색이 바뀐다. (이기면-초록, 지면-빨강, 비기면-검은색)
 
+
+//우리가 사용할 이미지를 저장한 객체 생성
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://scienceresourcebox.co.nz/cdn/shop/files/Chalkrounded_WEB_1200x1200.jpg?v=1684441843"
+  },
+  scissors: {
+    name: "Scissors",
+    img: "https://www.shutterstock.com/image-vector/3d-realistic-open-scissors-black-260nw-2376677033.jpg"
+  },
+  paper: {
+    name: "Paper",
+    img: "https://www.collinsdictionary.com/images/full/paper_111691001.jpg"
+
+  }
+}
+
+
+/* state에 있는 값이 변할 때 마다 UI 변함 */
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
+  const play=(userChoice)=>{
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    //console.log(computerChoice);
+    setComputerSelect(computerChoice);
+    judgement(choice[userChoice], computerChoice);
+  };
+
+  const judgement = (user, computer) => {
+    console.log("user", user, "computer", computer);
+  }
+
+  //객체에서 랜덤한 값을 추출하는 법  
+  const randomChoice=()=>{
+     let itemArray = Object.keys(choice); //객체에 키값만 뽑아서 어레이로 만들어주는 함수 
+     console.log("item array", itemArray);
+     let randomItem = Math.floor(Math.random()*itemArray.length); //random() => 0~1 사이의 랜덤한 숫자를 반환함
+    // ★★ Math.random() * itemArray.length의 결과는 0 이상 itemArray.length - 1 이하의 정수가 됨. 다시 말해, 0, 1, 2 같은 정수 값이 나올 수 있음.  
+    // ★★이 값은 배열의 유효한 인덱스가 되어 배열의 요소를 안전하게 참조할 수 있음.
+     let final = itemArray[randomItem]; //이렇게 하면 itemArray의 인덱스 번호에 담겨있는 name값이 출력됨
+     console.log("final",final);
+     return choice[final];
+  }
+
+
+
+/* 1. onclick 이벤트 사용할 때, 
+      함수를 바로 넣지 말고(play(X)), 콜백 함수 형태로 만들어줘야함(()=> play(O))  */
+/* 2. 전체를 div로 묶어주기 -> JSX 컴포넌트는 반드시 하나의 덩어리를 리턴해야함 */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className="main">
+        <Box title="You" item={userSelect} result={result}/>
+        <Box title="Computer" item={computerSelect} result={result}/>
+      </div>
+      <div className="btn">
+        <button onClick={()=>play("scissors")}>가위</button> 
+        <button onClick={()=>play("rock")}>바위</button>
+        <button onClick={()=>play("paper")}>보</button>
+      </div>
+  </div>  
   );
 }
 
---
+++ src/component/Box.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Box = (props) => {  
+  console.log("props",props);
+  return (
+    <div> 
+        <div className="box">
+            <h1>{props.title}</h1>
+            <img className="item-img" src={props.item && props.item.img}/>
+            {/* 초기값이 null이라면 props.item이라는 가드값을 넣어줘야함 
+                props.item이라는 값이 false면 애초에 실행을 안하기 때문
+                하지만, 유저가 넘겨준 item값이 있으면 true가 되면서
+                item에 있는 이미지 값을 불러오게 됨
+                */}
+            <h2>{props.result}</h2>
+        </div>        
+    </div>
+  )
+}
+
+export default Box
